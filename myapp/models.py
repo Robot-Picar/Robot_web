b2caa329b0c2168c70b7de542fbaa9aacf40f57b
Robot-Picar <ahtah20@gmail.com>
2024-04-29 02:33:27 +0300

    fix

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Robot(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Task(models.Model):
+    robot = models.ForeignKey(Robot, on_delete=models.CASCADE)
+    action = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
